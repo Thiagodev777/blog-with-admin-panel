6d57ee08a0957ba632c57d33913fa80f1c123ff7
Antonio Thiago <ladeiragrande777@gmail.com>
2022-10-28 02:06:01 -0300

    armazenamento de categorias no banco + slugify + tabela/categories

--- Controllers/categories/CategoriesController.js
+++ Controllers/categories/CategoriesController.js
@@ -1,14 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const slugify = require('slugify');
 
-router.get('/categories', (req, res)=>{
+// model Category
+const Category = require('../../Models/Category/Category');
+
+router.get('/admin/categories', (req, res)=>{
     res.statusCode = 200;
-    res.send('Rota de categoria')
+    res.render('admin/categories/index')
 });
+
+
 
 router.get('/admin/categories/new', (req, res)=>{
     res.statusCode = 200;
     res.render('admin/categories/new')
+})
+router.post('/categories/save', (req, res)=>{
+    let { title } = req.body;
+    if(title != undefined){
+        Category.create({
+            title: title,
+            slug: slugify(title)
+        }).then(() => {
+            res.redirect('/categories')
+        })
+    } else {
+        res.statusCode(308);
+        res.redirect('/admin/categories/new')
+    }
 })
 
 module.exports = router;
