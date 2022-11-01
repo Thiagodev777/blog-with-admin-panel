5e19421ab721790a38eac013be4f748d113dcf96
Antonio Thiago <ladeiragrande777@gmail.com>
2022-11-01 18:19:17 -0300

    definição das edições de Categoria

--- Controllers/categories/CategoriesController.js
+++ Controllers/categories/CategoriesController.js
@@ -37,20 +37,42 @@ router.post('/categories/save', (req, res)=>{
 router.post('/categories/delete', (req, res) => {
     let { id } = req.body
     if(id !== undefined){
-
         if(!isNaN(id)){
-
             Category.destroy({ where: { id: id } }).then(()=>{
                 res.redirect('/admin/categories')
             })
-
         }else {
             res.redirect('/admin/categories')
         }
-
-    }else {
-
     }
 })
+
+router.get("/admin/categories/edit/:id", (req, res)=>{
+    let { id } = req.params
+    if(isNaN(id)){
+        res.redirect('/admin/categories')
+    }
+    Category.findByPk(id).then((category) => { 
+        if(category != undefined){
+            res.render('admin/categories/edit', {
+                category: category
+            })
+        } else {
+            res.redirect('/admin/categories');
+        }
+     }).catch((err) => {
+        res.redirect('/admin/categories');
+     })
+})
+
+router.post('/categories/update', (req, res)=>{
+    let { id, title } = req.body
+    Category.update({title: title, slug: slugify(title)}, {
+        where: { id: id }
+    }).then(() => {
+        res.redirect('/admin/categories');
+    })
+})
+
 
 module.exports = router;
