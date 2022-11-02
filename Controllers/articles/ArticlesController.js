a3e0714b2ef4927277a979e264ee8abea527b1be
Antonio Thiago <ladeiragrande777@gmail.com>
2022-11-02 16:07:11 -0300

    definicao de rota de salvamento de artigo + ajuste timezone sequelize

--- Controllers/articles/ArticlesController.js
+++ Controllers/articles/ArticlesController.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const Category = require('../../Models/Category/Category')
 
-router.get('/articles', (req, res)=>{
+const Article = require('../../Models/Article/Article');
+const slugify = require('slugify');
+
+router.get('/admin/articles', (req, res)=>{
     res.statusCode = 200;
     res.send('Rota de Artigos')
 });
@@ -13,6 +16,18 @@ router.get('/admin/articles/new', (req, res)=>{
         res.render("admin/articles/new", {
             categories: categories
         })
+    })
+})
+
+router.post('/articles/save', (req, res)=>{
+    let { title, body, category } = req.body
+    Article.create({
+        title: title,
+        slug: slugify(title),
+        body: body,
+        categoryId: category
+    }).then(()=>{
+        res.redirect('/admin/articles')
     })
 })
 
--- config/database/conexao.js
+++ config/database/conexao.js
@@ -1,6 +1,7 @@
 const Sequelize = require('sequelize');
 const sequelize = new Sequelize(process.env.NAME_DATABASE, process.env.USERNAME_DATABASE, process.env.PASSWORD_DATABASE, {
     host: process.env.HOST_DATABASE,
-    dialect: process.env.TYPE_DATABASE
+    dialect: process.env.TYPE_DATABASE,
+    timezone: "-03:00"
 })
 module.exports = sequelize;
