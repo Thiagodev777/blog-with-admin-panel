b6615f8cc8a5dbb2aa7d3d55718e0c8526679e34
Antonio Thiago <ladeiragrande777@gmail.com>
2022-11-04 16:48:53 -0300

    ajustes de hash de senha

--- Controllers/users/UsersController.js
+++ Controllers/users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../../Models/User/User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
     res.send('Listagem de usuarios')
@@ -8,6 +9,22 @@ router.get('/admin/users', (req, res) => {
 
 router.get('/admin/users/create', (req, res) => {
     res.render('admin/users/create')
+})
+
+router.post('/users/create', (req, res) => {
+    let { email, password } = req.body;
+
+    let salt = bcrypt.genSaltSync(10);
+    let hash = bcrypt.hashSync(password, salt);
+    User.create({
+        email: email,
+        password: hash
+    }).then(() => {
+        res.redirect('/')
+    }).catch((err) => {
+        res.redirect('/')
+    })
+
 })
 
 
