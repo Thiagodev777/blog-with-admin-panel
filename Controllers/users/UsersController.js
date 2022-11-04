1ca6be203f16634201d005eb4489747bdb201293
Antonio Thiago <ladeiragrande777@gmail.com>
2022-11-04 17:04:02 -0300

    evitando emails duplicados no banco

--- Controllers/users/UsersController.js
+++ Controllers/users/UsersController.js
@@ -4,7 +4,11 @@ const User = require('../../Models/User/User');
 const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
-    res.send('Listagem de usuarios')
+    User.findAll({raw: true, order: [ ['id', 'DESC'] ]}).then((users) => {
+        res.render('admin/users/index', {
+            users, users
+        })
+    })
 })
 
 router.get('/admin/users/create', (req, res) => {
@@ -14,17 +18,25 @@ router.get('/admin/users/create', (req, res) => {
 router.post('/users/create', (req, res) => {
     let { email, password } = req.body;
 
-    let salt = bcrypt.genSaltSync(10);
-    let hash = bcrypt.hashSync(password, salt);
-    User.create({
-        email: email,
-        password: hash
-    }).then(() => {
-        res.redirect('/')
-    }).catch((err) => {
-        res.redirect('/')
-    })
 
+    User.findOne({where: {email: email}}).then((user) => {
+        if(user == undefined) {
+
+            let salt = bcrypt.genSaltSync(10);
+            let hash = bcrypt.hashSync(password, salt);
+            User.create({
+                email: email,
+                password: hash
+            }).then(() => {
+                res.redirect('/admin/users')
+            }).catch((err) => {
+                res.redirect('/')
+            })
+
+        } else {
+            res.redirect('/admin/users/create')
+        }
+    })
 })
 
 
